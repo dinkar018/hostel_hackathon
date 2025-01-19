7102f86ed64221808970cc2bbfb8f5193251cbab
dinkar018 <dinkarkumar7282@gmail.com>
2025-01-19 08:11:59 +0530

    sever created

--- server/index.js
+++ server/index.js
@@ -1,2 +1,18 @@
-import express from 'express'
-// Express
+const express= require('express');
+const app = express();
+require('dotenv').config();
+const connectDB = require('./db/connect');
+const port = process.env.PORT || 3000;
+const start = async () => {
+    try {
+      await connectDB(process.env.mongo_url);
+      console.log('connect to DB')
+      app.listen(port, () =>
+        console.log(`Server is listening on port ${port}...`)
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  
+  start();
