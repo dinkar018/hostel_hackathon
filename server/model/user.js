101a3f3971916ce766420606224e1f851fb09228
Akash chicnholkar <akash28052004@gmail.com>
2025-01-19 14:58:20 +0530

    4

--- server/model/user.js
+++ server/model/user.js
@@ -1,12 +1,41 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.chema{(
-           id:{
-            type:String;
-            unique:true;
-           }
+const userSchema = new mongoose.Schema({
+ id:{
+        type:String,
+        unique:true,
+        required:true,
+       },
+ hostel:{
+           type:String,
+           required:true,
+ },
+contact:{
+    type:String,
+    required:true,
+    unique:true,
+},
+domain:{
+    type:String,
+    required:true,
+
+},
+details:{
+   type:String,
+   required:true,
+},
+urgency:{
+   type:String,
+   required:true,
+}
 
 
 
 
-)}
+
+})
+          
+
+
+
+
