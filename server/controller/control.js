f912fae1303520480cd2e247cf9925849542e9dd
dinkar018 <dinkarkumar7282@gmail.com>
2025-01-19 11:27:35 +0530

    apis addes

--- server/controller/control.js
+++ server/controller/control.js
@@ -8,6 +8,54 @@ const getAllComplains= async (req,res)=>{
         res.status(500).json({ msg: error})
     }
 }
+const getCivilComplains = async (req,res)=>{
+    try{
+        const temp = await Task.find({ domain : "civil"})
+        res.status(200).json({ temp })
+    }catch(error){
+        res.status(500).json({msg: error})
+    }
+}
+const getElectricalComplains = async (req,res)=>{
+    try{
+        const temp = await Task.find({ domain : "electrical"})
+        res.status(200).json({ temp })
+    }catch(error){
+        res.status(500).json({msg: error})
+    }
+}
+const getPlumbingComplains = async (req,res)=>{
+    try{
+        const temp = await Task.find({ domain : "plumbing"})
+        res.status(200).json({ temp })
+    }catch(error){
+        res.status(500).json({msg: error})
+    }
+}
+const getWaterCoolerComplains = async (req,res)=>{
+    try{
+        const temp = await Task.find({ domain : "watercooler"})
+        res.status(200).json({ temp })
+    }catch(error){
+        res.status(500).json({msg: error})
+    }
+}
+const getCompletedComplains = async (req,res)=>{
+    try{
+        const temp = await Task.find({ status : "completed"})
+        res.status(200).json({ temp })
+    }catch(error){
+        res.status(500).json({msg: error})
+    }
+}
+const getActiveComplains = async (req,res)=>{
+    try{
+        const temp = await Task.find({ status : "active"})
+        res.status(200).json({ temp })
+    }catch(error){
+        res.status(500).json({msg: error})
+    }
+}
 const createComplain = async (req,res)=>{
     body=req.body
     if(!body) res.status(404).json("please input all the fields")
@@ -31,5 +79,5 @@ const createComplain = async (req,res)=>{
 }
 
 module.exports ={
-    getAllComplains,createComplain
+    getAllComplains,createComplain,getCivilComplains,getElectricalComplains,getPlumbingComplains,getWaterCoolerComplains,getCompletedComplains,getActiveComplains
 }
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,8 +1,14 @@
 const express= require('express');
 const router = express.Router();
 
-const { getAllComplains,createComplain } = require('../controller/control');
+const { getAllComplains,createComplain,getCivilComplains,getElectricalComplains,getPlumbingComplains,getWaterCoolerComplains,getCompletedComplains,getActiveComplains } = require('../controller/control');
 
 router.route('/').get(getAllComplains).post(createComplain);
+router.route('/civil').get(getCivilComplains);
+router.route('/electrical').get(getElectricalComplains);
+router.route('/plumbing').get(getPlumbingComplains);
+router.route('/watercooler').get(getWaterCoolerComplains);
+router.route('/completed').get(getCompletedComplains);
+router.route('/active').get(getActiveComplains);
 
 module.exports= router
